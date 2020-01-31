43de3a7042b419fd030e1af794a8016977f6f09d
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2020-01-31 07:04:12 +0100

    Revert "fix regex"
    This reverts commit b1d75c56dcee2577f1b14ef3173b70f8b7102847.

--- plugins/image.js
+++ plugins/image.js
@@ -40,7 +40,12 @@ plugin.main = function (teamspeak) {
         });
 
         function validURL(str) {
-            var pattern = new RegExp('[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)','i'); // URL Regex
+            var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+              '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+              '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+              '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+              '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+              '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
             return !!pattern.test(str);
           }
 
