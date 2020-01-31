4174e2a5bcf987dd5ac170eb27ef19bfc8dae871
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2020-01-31 05:51:07 +0100

    First Working Version

--- index.js
+++ index.js
@@ -8,57 +8,42 @@ var path = require('path');
 (function () {
     const fs = require("fs");
     var files = fs.readdirSync("./plugins");
-    var pluginArr = [];
     files.forEach(file => {
         if (path.extname(file) != '.js') return;
         var plugin;
         try {
             log("Loading plugin: " + file)
             plugin = require("./plugins/" + file);
-            initPlugin(plugin,pluginArr);
+            initPlugin(plugin)
         } catch (e) {
             log("Error while requiring a new plugin: " + file, false);
             console.log(e);
         }
-    });
-})();
 
+
+    })
+})();
 
 log("Connecting to TS3 Query Server", true);
 
-function initPlugin(plugin,array) {
-    var handle = false;
-    for(let i=0;i<array.length;i++){
-        if(array[i] && (array[i].sid == plugin.activeServerId)){
-            handle = array[i].handle;
-            break;
-        }
-    }
-    
-    if(!handle){
-        const { TeamSpeak } = require("ts3-nodejs-library");
-        TeamSpeak.connect({
-            host: config.Query.host,
-            queryport: config.Query.port, //optional
-            //serverport: 9987,
-            username: config.Query.username,
-            password: config.Query.password,
-            nickname: "test"
-        }).then(async teamspeak => {
-            teamspeak.useBySid(plugin.activeServerId).then(async () => {
-                plugin.main(teamspeak);
-            });
-    
-        }).catch(e => {
-            console.log("An error occured while trying to connect to TS3 Query!")
-            console.error(e)
+function initPlugin(plugin) {
+    const { TeamSpeak } = require("ts3-nodejs-library");
+    TeamSpeak.connect({
+        host: config.Query.host,
+        queryport: config.Query.port, //optional
+        //serverport: 9987,
+        username: config.Query.username,
+        password: config.Query.password,
+        nickname: "test"
+    }).then(async teamspeak => {
+        teamspeak.useBySid(plugin.activeServerId).then(async () => {
+            plugin.main(teamspeak);
         });
 
-        array.push({sid:plugin.activeServerId,handle:TeamSpeak})
-
-    }else{
-        plugin.main(handle);
-    }
+    }).catch(e => {
+        console.log("An error occured while trying to connect to TS3 Query!")
+        console.error(e)
+    });
 }
 
 //Log Function
