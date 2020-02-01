7133ea86da91d1b7fb24bca84359b35db35b5370
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2020-02-02 00:49:39 +0100

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [{
+    name: 'TeamCommand',
+    script: 'index.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: '',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
+    env_webhook: {
+      port: 27777,
+      path: "/teamcommand",
+      secret: "nonono",
+      action: "pullAndReload",
+  }
+  }]
+};
